55a1098208fedfad9ae89689e6803d2b03288822
Francisco Vallarino <fjvallarino@users.noreply.github.com>
2019-07-04 18:47:03 -0300

    Implement example component (#1)
    * Initial commit
    
    * Copy .circle folder
    
    * Add eslint files
    
    * Add eslint-config-idiomatic dependency
    
    * Remove idiomatic dependency
    
    * Restore value property
    
    * Add config files
    
    * Remove config files

---
+++ src/rise-example-component.js
@@ -0,0 +1,50 @@
+import {PolymerElement, html} from "@polymer/polymer";
+
+export default class RiseExampleComponent extends PolymerElement {
+
+  static get template() {
+    return html`[[value]]`;
+  }
+
+  static get properties() {
+    return {
+      value: {
+        type: String
+      }
+    }
+  }
+
+  // Event name constants
+  static get EVENT_CONFIGURED() {
+    return "configured";
+  }
+
+  constructor() {
+    super();
+  }
+
+  ready() {
+    super.ready();
+
+    if (RisePlayerConfiguration.isConfigured()) {
+      this._init();
+    } else {
+      window.addEventListener( "rise-components-ready", () => this._init(), { once: true });
+    }
+  }
+
+  _init() {
+    this._sendEvent(RiseExampleComponent.EVENT_CONFIGURED);
+  }
+
+  _sendEvent(eventName, detail = {}) {
+    const event = new CustomEvent(eventName, {
+      bubbles: true, composed: true, detail
+    });
+
+    this.dispatchEvent(event);
+  }
+
+}
+
+customElements.define("rise-example-component", RiseExampleComponent);
